1c2f39df35aecf0ee18db0ae6c1604a0f2a365bf
gt23669 <33846502+gt23669@users.noreply.github.com>
2021-01-24 18:02:13 -0700

    Add project files.

---
+++ ScratchProject_Chess/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ScratchProject_Chess
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
